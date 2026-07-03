480cdd6d414bb3034c0c10cb96e9678576858567
Michael Weibel <mweibel@cloudscale.ch>
2026-07-03 15:15:40 +0200

    feat: track observed generation
    Adds tracking of the observed generation into status.observedGeneration
    
    Rationale: https://github.com/kubernetes-sigs/cluster-api/issues/3078

--- api/v1beta2/cloudscalemachine_types.go
+++ api/v1beta2/cloudscalemachine_types.go
@@ -130,6 +130,11 @@ type MachineInitializationStatus struct {
 
 // CloudscaleMachineStatus defines the observed state of CloudscaleMachine.
 type CloudscaleMachineStatus struct {
+	// observedGeneration is the latest generation observed by the controller.
+	// +optional
+	// +kubebuilder:validation:Minimum=1
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
 	// Initialization contains v1beta2 initialization tracking.
 	// +optional
 	Initialization *MachineInitializationStatus `json:"initialization,omitempty"`
